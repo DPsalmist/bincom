3de9e7703f14a0c524543a03f355887fd6183318
Samuel Damilare <48826043+DPsalmist@users.noreply.github.com>
2021-01-09 05:40:36 +0100

    Deleted a redundant url

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-#from .views import HomeView, SearchResultsView
+from .views import HomeView, SearchResultsView
 from . import views
 
 urlpatterns = [
 	path('', views.HomeView.as_view(), name='index'),
 	path('create_poll/', views.createPoll, name='create'),
 	path('total_results/', views.totalResults, name='total_results'),
-	#path('search/', views.search_results, name='search_results'),
 	path('search/', views.SearchResultsView.as_view(), name='search_results'),
 ]
